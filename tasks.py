4b4ceea3ad28f14ceb611ee508219ab1e021b4e6
Nakarin Hansawattana <nakarinh14@gmail.com>
2022-07-14 21:22:17 +0700

    Add OTL context propagation

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 from celery.signals import worker_process_init
 from celery.utils.log import get_task_logger
-from opentelemetry import trace
+from opentelemetry import trace, propagators
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.celery import CeleryInstrumentor
 from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
@@ -32,8 +32,9 @@ try:
 except:
     pass
 
-
 tracer = None
+
+
 @worker_process_init.connect(weak=False)
 def init_celery_tracing(*args, **kwargs):
     global tracer
@@ -58,63 +59,65 @@ logger = get_task_logger(__name__)
 
 
 @app.task(name=EventStatus.CREATE_ORDER, bind=True)
-def create_order(self, buyer_id, product_id, order_id):
-    with tracer.start_span(name="update task to STARTED status"):
-        self.update_state(state='STARTED')
-    logger.info(f"Receive Buyer ID: {buyer_id}, Product ID: {product_id}, Order ID: {order_id}")
-    db_session = Session()
+def create_order(self, buyer_id, product_id, order_id, context_payload):
+    ctx = propagators.extract(_header_from_carrier, context_payload)
+    with tracer.start_as_current_span("SAGA create_order", context=ctx):
+        with tracer.start_span(name="update task to STARTED status"):
+            self.update_state(state='STARTED')
+        logger.info(f"Receive Buyer ID: {buyer_id}, Product ID: {product_id}, Order ID: {order_id}")
+        db_session = Session()
 
-    event_record = db_session.query(ProcessedEvent).filter(and_(
-        ProcessedEvent.chain_id == order_id,
-        ProcessedEvent.event == EventStatus.CREATE_ORDER,
-    )).first()
-    db_session.commit()
+        event_record = db_session.query(ProcessedEvent).filter(and_(
+            ProcessedEvent.chain_id == order_id,
+            ProcessedEvent.event == EventStatus.CREATE_ORDER,
+        )).first()
+        db_session.commit()
 
-    payload = {'order_id': order_id, 'product_id': product_id, 'buyer_id': buyer_id, 'step': 0}
-    # If event is already processed, we skip the event processing
-    # but fire the next event just in-case the next-published message is lost
-    if event_record is not None:
-        with tracer.start_span(name=f"send_task {event_record.next_event}"):
-            app.send_task(
-                event_record.next_event,
-                kwargs=payload,
-                queue=EventStatus.get_queue(event_record.next_event),
-            )
-            return payload
+        payload = {'order_id': order_id, 'product_id': product_id, 'buyer_id': buyer_id, 'step': 0}
+        # If event is already processed, we skip the event processing
+        # but fire the next event just in-case the next-published message is lost
+        if event_record is not None:
+            with tracer.start_span(name=f"send_task {event_record.next_event}"):
+                app.send_task(
+                    event_record.next_event,
+                    kwargs=payload,
+                    queue=EventStatus.get_queue(event_record.next_event),
+                )
+                return payload
 
-    order = Order(
-        uuid=order_id,
-        status=OrderStatus.INIT,
-        buyer_id=buyer_id,
-        product_id=product_id,
-    )
-    history = ProcessedEvent(
-        chain_id=order_id,
-        event_id=self.request.id,
-        event=EventStatus.CREATE_ORDER,
-        next_event=EventStatus.UPDATE_PRODUCT_QUOTA,
-        step=0
-    )
+        order = Order(
+            uuid=order_id,
+            status=OrderStatus.INIT,
+            buyer_id=buyer_id,
+            product_id=product_id,
+        )
+        history = ProcessedEvent(
+            chain_id=order_id,
+            event_id=self.request.id,
+            event=EventStatus.CREATE_ORDER,
+            next_event=EventStatus.UPDATE_PRODUCT_QUOTA,
+            step=0
+        )
 
-    transaction_success = False
+        transaction_success = False
 
-    with tracer.start_span(name="Execute DB Transaction"):
-        try:
-            with db_session.begin():
-                db_session.add_all([order, history])
-            transaction_success = True
-        except Exception as e:
-            logger.error(e)
-            logger.info(f"{EventStatus.CREATE_ORDER} failed for Buyer ID: {buyer_id} Product ID: {product_id}")
-    # Since this is the origin of SAGA, no need to revert event when transaction failed
-    if transaction_success:
-        with tracer.start_span(name=f"send_task {EventStatus.UPDATE_PRODUCT_QUOTA}"):
-            app.send_task(
-                EventStatus.UPDATE_PRODUCT_QUOTA,
-                kwargs=payload,
-                queue=EventStatus.get_queue(EventStatus.UPDATE_PRODUCT_QUOTA),
-            )
-    return payload
+        with tracer.start_span(name="Execute DB Transaction"):
+            try:
+                with db_session.begin():
+                    db_session.add_all([order, history])
+                transaction_success = True
+            except Exception as e:
+                logger.error(e)
+                logger.info(f"{EventStatus.CREATE_ORDER} failed for Buyer ID: {buyer_id} Product ID: {product_id}")
+        # Since this is the origin of SAGA, no need to revert event when transaction failed
+        if transaction_success:
+            with tracer.start_span(name=f"send_task {EventStatus.UPDATE_PRODUCT_QUOTA}"):
+                app.send_task(
+                    EventStatus.UPDATE_PRODUCT_QUOTA,
+                    kwargs=payload,
+                    queue=EventStatus.get_queue(EventStatus.UPDATE_PRODUCT_QUOTA),
+                )
+        return payload
 
 
 @app.task(name=EventStatus.REVERT_CREATE_ORDER, bind=True)
@@ -122,3 +125,8 @@ def revert_create_order(self, order_uuid):
     # TODO: Work on remove order
     self.update_state(state='STARTED')
     return True
+
+
+def _header_from_carrier(carrier, key):
+    header = carrier.get(key)
+    return [header] if header else []
